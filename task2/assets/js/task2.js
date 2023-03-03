b6c019919a1613852cf286a22273cadc5affbafa
Mahir <mahirgs@code.edu.az>
2023-03-04 01:38:17 +0400

    lineCOlor

--- task2/assets/js/task2.js
+++ task2/assets/js/task2.js
@@ -5,21 +5,29 @@ imgs = document.querySelectorAll("img")
 
 function rightSlider() {
     let activateImage = document.querySelector(".show");
+    let opa = document.querySelector(".opa")
     activateImage.classList.remove("show");
+    opa.classList.remove("opa")
     if (activateImage.nextElementSibling != null) {
         activateImage.nextElementSibling.classList.add("show")
+        opa.nextElementSibling.classList.add("opa")
     } else {
         activateImage.parentNode.firstElementChild.classList.add("show")
+        opa.parentNode.firstElementChild.classList.add("opa")
     }
 }
 
 function leftSlider() {
     let activateImage = document.querySelector(".show");
+    let opa = document.querySelector(".opa")
     activateImage.classList.remove("show");
+    opa.classList.remove("opa")
     if (activateImage.previousElementSibling != null) {
         activateImage.previousElementSibling.classList.add("show")
+        opa.previousElementSibling.classList.add("opa")
     } else {
         activateImage.parentNode.lastElementChild.classList.add("show")
+        opa.parentNode.lastElementChild.classList.add("opa")
     }
 }
 imgs.forEach(img => {
@@ -31,8 +39,12 @@ imgs.forEach(img => {
             else {
                 img.classList.remove("show");
             }
+            let opa = document.querySelector(".opa");
+            opa.classList.remove("opa")
+            this.classList.add("opa")
         })
     });
 });
+setInterval(rightSlider, 5000);
 right.addEventListener("click", rightSlider);
 left.addEventListener("click", leftSlider);
